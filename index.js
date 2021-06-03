261fdbf6c969adeb8b0576719785e598b1cfcd25
mahmudsajib <mahmudsajib082@gmail.com>
2021-06-03 11:36:47 +0600

    reverse

---
+++ index.js
@@ -0,0 +1,20 @@
+// const arr = [1,2,3,4,5];
+
+// // function reverce(){
+// //   for(var i=0; i<arr; i++){
+// //     return arr[i]
+// //   }
+// // }
+// // console.log( reverce[arr(i)])
+// const result= arr.reverse()
+// console.log(result)
+function multiTable() {
+  const number = 5
+  
+  for(let i=0; i<=10; i++){
+    // const result//
+     const result =   number * i +'\n'
+  }
+}
+
+console.log(multiTable(result))
